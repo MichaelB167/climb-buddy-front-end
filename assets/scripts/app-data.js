99df80576b972abc10c5a1749627508993b5fbab
MichaelB167 <michael.m.bates@gmail.com>
2016-05-03 09:48:50 -0400

    Update app-data for heroku api

--- assets/scripts/app-data.js
+++ assets/scripts/app-data.js
@@ -1,7 +1,9 @@
 'use strict';
 
 const app = {
-  api: 'http://localhost:3000',
+//  api: 'http://localhost:3000',
+//  };
+  api: 'https://climb-buddy.herokuapp.com'
   };
 
 module.exports =  app;
